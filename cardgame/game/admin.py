f3d94e4c914e5142d5cd731c8aaa58b06a045f85
Sylouan Corfa <t4raal@gmail.com>
2022-09-04 16:02:42 +0200

    Added a customUser model + forms/admin settings

--- cardgame/game/admin.py
+++ cardgame/game/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import MainUserChangeForm, MainUserCreationForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = MainUserCreationForm
+    form = MainUserChangeForm
+    model = CustomUser
+    list_display = ["email", "username", ]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- cardgame/game/models.py
+++ cardgame/game/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(db_index=True, default=timezone.now)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Card(BaseModel):
+    health = models.IntegerField()
+    attack = models.IntegerField()
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class CustomUser(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
